d9b592a389bd1e6fcc98176add452485d04d725b
Brian Pérez López <brianpl990227@gmail.com>
2023-04-28 23:50:42 -0400

    Creación de cards y grid

---
+++ components/PetCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <Card>
+        <template #header>
+            <img class="br-5" alt="user header" src="/img/auth.jpg" />
+        </template>
+        <template #title> Advanced Card </template>
+        <template #subtitle> Card subtitle </template>
+        <template #content>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+                cupiditate neque
+                quas!
+            </p>
+        </template>
+        <template #footer>
+            <Button icon="pi pi-check" label="Save" />
+            <Button icon="pi pi-times" label="Cancel" severity="secondary" style="margin-left: 0.5em" />
+        </template>
+    </Card>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+useHead({
+    title: 'Mascotas'
+})
+
+
+</script>
--- pages/pet/index.vue
+++ pages/pet/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Pets</h1>
+    
+    <div class="grid p-20">
+        <div class="col-12 lg:col-3 md:col-6 " v-for="item in test">
+            <PetCard></PetCard>
+        </div>
+    </div>
+
     <NuxtLink class="none-decoration" to="/pet/create">
         <Button color="primary" class="p-fixed" style="bottom: 0; right: 0; margin: 50px;" icon="pi pi-plus" raised rounded
             aria-label="Filter" />
@@ -8,5 +14,7 @@
 
 <script setup>
 
+
+const test = [1,2,3,4,5,6,7,8,9]
 
 </script>
