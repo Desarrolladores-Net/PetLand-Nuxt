f9084a7c30026ba25b45c70e09e4964ddf3460f3
Brian Pérez López <brianpl990227@gmail.com>
2023-06-07 20:10:28 -0400

    Editar preguntas

--- pages/setting/[id]/questions/index.vue
+++ pages/setting/[id]/questions/index.vue
@@ -25,7 +25,7 @@
                     <Column header="Acciones">
                         <template #body="{ data }">
                             <Button text v-tooltip.bottom="'Editar pregunta'" icon="pi pi-pencil"
-                               severity="success"></Button>
+                               severity="success" @click="editQuestion({...data})"></Button>
                             <Button text v-tooltip.bottom="'Eliminar pregunta'" icon="pi pi-trash"
                                 severity="danger" @click="confirmDelete(data.id)"></Button>
                         </template>
@@ -63,12 +63,32 @@
                 animationDuration=".5s" aria-label="Custom ProgressSpinner" />
         </div>
     </Dialog>
+    <Dialog v-model:visible="editDialog" modal header="Editar pregunta" :style="{ width: '90vh' }">
+        <div class="p-20">
+            <span class="p-float-label">
+                <InputText class="p-inputtext-sm" v-model="editQuestionModel.message" focusable style="width: 100%;" />
+                <label for="username">Pregunta</label>
+            </span>
+            <div class="p-float-label mt-10" style="width: 100%;">
+                <Dropdown v-model="editQuestionModel.typeQuestion" input-class="full-width" inputId="dd-city"
+                    :options="[{ name: 'Respuesta simple', value: 0 }, { name: 'Respuesta larga', value: 1 }, { name: 'Sí o no', value: 2 }]"
+                    optionLabel="name" placeholder="Seleccione el tipo de pregunta" class="w-full" />
+                <label for="dd-city">Tipo de pregunta</label>
+            </div>
+        </div>
+        <div class="d-flex just-content-right">
+            <Button label="Cancelar" class="mr-10" text severity="secondary" @click="closeEditDialog()"></Button>
+            <Button v-if="!loading.edit" label="Guardar" @click="editQuestionRequest()" class="mr-10"></Button>
+            <ProgressSpinner v-else style="height: 30px" strokeWidth="8" fill="var(--surface-ground)"
+                animationDuration=".5s" aria-label="Custom ProgressSpinner" />
+        </div>
+    </Dialog>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { useRoute } from "vue-router";
-import { CreateQuestionRequest, GetQuestionRequest, DeleteQuestionRequest } from '../../../../services/question/QuestionService';
+import { CreateQuestionRequest, GetQuestionRequest, DeleteQuestionRequest, GetStringTypeQuestion, EditQuestionRequest } from '../../../../services/question/QuestionService';
 import { useToast } from "primevue/usetoast";
 import { useConfirm } from "primevue/useconfirm";
 
@@ -76,7 +96,9 @@ const confirm = useConfirm()
 const toast = useToast()
 const createDialog = ref(false)
 const createQuestionModel = ref({ message: '', typeQuestion: { name: '', value: '' } })
-const loading = ref({ create: false })
+const editDialog = ref(false)
+const editQuestionModel = ref()
+const loading = ref({ create: false, edit: false })
 const route = useRoute()
 const { data: questions } = await useAsyncData('questions', () => GetQuestionRequest(route.params.id))
 
@@ -98,27 +120,64 @@ const createQuestionRequest = async () => {
     toast.add({ severity: 'info', summary: 'Pregunta creada', detail: 'Ha guardado una pregunta en el sistema.', life: 4000 });
 }
 
+const editQuestionRequest = async () => {
+
+    loading.value.edit = true
+
+    const dto = {
+        id: editQuestionModel.value.id,
+        message: editQuestionModel.value.message,
+        typeQuestion: editQuestionModel.value.typeQuestion.value
+    }
+
+    const response = await EditQuestionRequest(dto)
+
+    questions.value = await GetQuestionRequest(route.params.id)
+
+    loading.value.edit = false
+
+    editDialog.value = false
+
+
+}
+
 const closeCreateDialog = () => {
     createDialog.value = false
     createQuestionModel.value.typeQuestion = ''
     createQuestionModel.value.message = ' '
 }
 
+const editQuestion = (data) => {
+    
+    const typeQuestion = {
+        name: '',
+        value: data.typeQuestion
+    }
+
+    editQuestionModel.value = unref(data)
+    editQuestionModel.value.typeQuestion = typeQuestion
+
+    editQuestionModel.value.typeQuestion.name = GetStringTypeQuestion(data.typeQuestion)
+    editDialog.value = true
+}
+
+const closeEditDialog = () => {
+    editQuestionModel.value = ''
+    editDialog.value = false
+}
+
 const confirmDelete = (id) => {
     confirm.require({
-        message: '¿Desea borrar esta pregunta?',
+        message: '¿Quieres borrar esta pregunta?',
         header: 'Confirmación',
         icon: 'pi pi-info-circle',
         acceptLabel: 'Sí',
         rejectLabel: 'No',
         acceptClass: 'p-button-danger',
         accept: async () => {
-
-            const response = await DeleteQuestionRequest(id)
-            
+            const response = await DeleteQuestionRequest(id)  
             questions.value = await GetQuestionRequest(route.params.id)
-
-            toast.add({ severity: 'info', summary: 'Formulario eliminado', detail: 'Esta pregunta se ha eliminado del sistema.', life: 3000 });
+            toast.add({ severity: 'info', summary: 'Pregunta eliminada', detail: 'Esta pregunta se ha eliminado del sistema.', life: 3000 });
         }
     });
 };
--- services/question/QuestionService.ts
+++ services/question/QuestionService.ts
@@ -2,17 +2,32 @@ import Axios from "axios";
 import { CreateInterfaceRM, QuestionItem } from "@/interfaces/question/interface";
 import { QuestionBaseUrl } from "~~/helpers/url";
 
-export function CreateQuestionRequest(dto: CreateInterfaceRM) : Promise<QuestionItem>
-{
+export function CreateQuestionRequest(dto: CreateInterfaceRM): Promise<QuestionItem> {
     return Axios.post(QuestionBaseUrl, dto).then(response => response.data)
 }
 
-export function GetQuestionRequest(formId: String) : Promise<Array<QuestionItem>>
-{
-    return Axios.get(QuestionBaseUrl+'/'+formId).then(response => response.data)
+export function GetQuestionRequest(formId: String): Promise<Array<QuestionItem>> {
+    return Axios.get(QuestionBaseUrl + '/' + formId).then(response => response.data)
 }
 
-export function DeleteQuestionRequest(id: String) : Promise<QuestionItem>
-{
-    return Axios.delete(QuestionBaseUrl+'/'+id).then(response => response.data)
+export function DeleteQuestionRequest(id: String): Promise<QuestionItem> {
+    return Axios.delete(QuestionBaseUrl + '/' + id).then(response => response.data)
+}
+
+export function EditQuestionRequest(dto: CreateInterfaceRM): Promise<QuestionItem> {
+
+    return Axios.put(QuestionBaseUrl, dto).then(response => response.data)
+
+}
+
+export function GetStringTypeQuestion(type: number): String {
+    if (type == 0) {
+        return 'Respuesta simple'
+    }
+    else if (type == 1) {
+        return 'Respuesta larga'
+    }
+    else {
+        return 'Sí o no'
+    }
 }
